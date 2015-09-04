195a2acd20df3b40b60ec66ce1e3de115e49bc56
iahvector <iah.vector@gmail.com>
2015-09-04 20:33:46 +0200

    Move user time line to a separarte end point and move auth url to a separate namespace

--- twitterclient_core/urls.py
+++ twitterclient_core/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signin$', views.signIn, name='signIn'),
-    url(r'^twitter_callback$', views.twitter_callback, name='twitter_callback'),
+    url(r'^auth/signin$', views.signIn, name='signIn'),
+    url(r'^auth/twitter_callback$', views.twitter_callback,
+        name='twitter_callback'),
+    url(r'^time_line$', views.user_time_line, name='user_time_line')
 ]
--- twitterclient_core/views.py
+++ twitterclient_core/views.py
@@ -7,7 +7,8 @@ from django.core.urlresolvers import reverse
 request_token_url = 'https://api.twitter.com/oauth/request_token'
 base_authorization_url = 'https://api.twitter.com/oauth/authenticate'
 access_token_url = 'https://api.twitter.com/oauth/access_token'
-verify_account_url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
+verify_account_url = 'https://api.twitter.com/1.1/account\
+/verify_credentials.json'
 user_time_line_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
 twitter_consumer_key = settings.TWITTER_CONSUMER_KEY
 twitter_secret_Key = settings.TWITTER_CONSUMER_SECRET
@@ -20,15 +21,13 @@ def index(request):
 def signIn(request):
     callback_url = request.build_absolute_uri(reverse('twitter_callback'))
 
-    print callback_url
-
     oauth = OAuth1Session(twitter_consumer_key,
                           client_secret=twitter_secret_Key,
                           callback_uri=callback_url)
     response = oauth.fetch_request_token(request_token_url)
 
-    request.session['resource_owner_key'] = response.get('oauth_token')
-    request.session['resource_owner_secret'] = response.get(
+    request.session['oauth_token'] = response.get('oauth_token')
+    request.session['oauth_token_secret'] = response.get(
         'oauth_token_secret')
 
     authorization_url = oauth.authorization_url(base_authorization_url)
@@ -37,20 +36,20 @@ def signIn(request):
 
 
 def twitter_callback(request):
-    resource_owner_key = request.session['resource_owner_key']
-    resource_owner_secret = request.session['resource_owner_secret']
+    oauth_token = request.session['oauth_token']
+    oauth_token_secret = request.session['oauth_token_secret']
 
-    # check if resource_owner_key & resource_owner_secret exist
+    # check if oauth_token & oauth_token_secret exist
     # else redirect to restart the sign in process
 
     oauth = OAuth1Session(twitter_consumer_key,
                           client_secret=twitter_secret_Key,
-                          resource_owner_key=resource_owner_key,
-                          resource_owner_secret=resource_owner_secret)
+                          resource_owner_key=oauth_token,
+                          resource_owner_secret=oauth_token_secret)
 
     redirect_response = request.build_absolute_uri(request.get_full_path())
     oauth_response = oauth.parse_authorization_response(redirect_response)
-    verifier = oauth_response.get('oauth_verifier')
+    oauth_response.get('oauth_verifier')
     oauth_tokens = oauth.fetch_access_token(access_token_url)
     request.session['oauth_token'] = oauth_tokens.get('oauth_token')
     request.session['oauth_token_secret'] = oauth_tokens.get(
@@ -60,18 +59,31 @@ def twitter_callback(request):
 
     user = verify_account_response.json()
 
-    print user
+    request.session['user_id'] = user['id_str']
+    request.session['user_name'] = user['name']
+    request.session['user_screen_name'] = user['screen_name']
+    request.session['user_profile_image_url'] = user['profile_image_url']
 
-    user_id = user['id_str']
-    user_name = user['name']
-    user_screen_name = user['screen_name']
-    user_profile_image_url = user['profile_image_url']
+    return JsonResponse(user, safe=False)
 
-    user_time_line_response = oauth.get(user_time_line_url,
-                                        params={'user_id': user_id,
-                                                'count': 5, 'trim_user': True})
-    time_line = user_time_line_response.json()
 
-    print time_line
+def user_time_line(request):
+    oauth_token = request.session['oauth_token']
+    oauth_token_secret = request.session['oauth_token_secret']
+    user_id = request = request.session['user_id']
+
+    # Check oauth_token, oauth_token_secret & user_id
+
+    oauth = OAuth1Session(twitter_consumer_key,
+                          client_secret=twitter_secret_Key,
+                          resource_owner_key=oauth_token,
+                          resource_owner_secret=oauth_token_secret)
+
+    # get count from request params
+    time_line_response = oauth.get(user_time_line_url,
+                                   params={'user_id': user_id,
+                                           'count': 5,
+                                           'trim_user': True})
+    time_line = time_line_response.json()
 
     return JsonResponse(time_line, safe=False)
